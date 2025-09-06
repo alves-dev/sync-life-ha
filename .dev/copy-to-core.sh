b149483de698bd97ac33b6c344a95b01acd32c34
alves-dev <alvesmoreiraigor@gmail.com>
2025-09-06 11:01:23 -0300

    ci: up in dev

---
+++ .dev/copy-to-core.sh
@@ -0,0 +1,2 @@
+sudo rm -r /home/igor/projects/ha-core/config/custom_components/synclife/*
+sudo cp -r /home/igor/projects/sync-life-ha/custom_components/synclife/* /home/igor/projects/ha-core/config/custom_components/synclife/
